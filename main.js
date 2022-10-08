b6eff7c4306212ac84b6d941802282995ae0550c
Javohirjewel <shdomonovjavohir56@gmail.com>
2022-10-08 14:18:16 +0500

    change all

---
+++ main.js
@@ -0,0 +1,9 @@
+alert('Salom foydalanuvchi')
+
+        
+        let ism = prompt('Ismingiz')
+        console.log('Ismingiz ' + ism);
+        let familiya = prompt('Familiyangiz')
+        console.log('Familiyangiz ' + familiya);
+        let age = prompt('Yoshingiz')
+        console.log('Yoshingiz '+ age+'da');

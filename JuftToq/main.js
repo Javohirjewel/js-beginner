26648f2d6e078d7237d943312af9bb36a961d899
Javohirjewel <shdomonovjavohir56@gmail.com>
2022-10-10 23:29:09 +0500

    finished

---
+++ JuftToq/main.js
@@ -0,0 +1,11 @@
+let a=+prompt('son kiriting');
+
+if(typeof(a) === 'number'){
+        if(a%2===0){
+            alert("juft son");
+        }else{
+            alert('toq son');
+        }
+}else{
+    alert("Oka son kiritvoren");
+}

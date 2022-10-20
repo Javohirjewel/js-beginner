8dc3be4d87eca172c14a575835d082a9372eff03
Javohirjewel <shdomonovjavohir56@gmail.com>
2022-10-20 15:19:38 +0500

    first

--- CardNumber/main.js
+++ CardNumber/main.js
@@ -1,7 +1,21 @@
-let CardNumber=parseInt(prompt("karta raqamini kiriting"), 10);
+let cardNum=prompt("karta raqamini kiriting")
+let cardSec = cardNum.slice(0, 4)+ "********" + cardNum.slice(12, 16);
+function cardNumber(cardNum){
+    if(cardNum.length===16 && !isNaN(+cardNum)){
 
-if(CardNumber.length===16){
-    alert("True card number");
-}else{
-    alert('short or long card number')
+        if(cardNum.startsWith("8600")){
+            console.log("UzCard plastik ishlatasiz")
+            console.log(cardSec)
+        } else if(cardNum.startsWith("9860")){
+            console.log("Humo plastik ishlatasiz")
+            console.log(cardSec)
+        }else{
+            console.log("Xato karta kirittez")
+        }  
+    } 
+    else{
+        console.log('short or long card number')
+    }
 }
+cardNumber(cardNum)
+

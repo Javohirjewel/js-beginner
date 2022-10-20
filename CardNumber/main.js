a72f15c18c4f31e947c83b522cbcb0ea0922b7ff
Javohirjewel <shdomonovjavohir56@gmail.com>
2022-10-20 18:16:47 +0500

    changed 2

--- CardNumber/main.js
+++ CardNumber/main.js
@@ -1,7 +1,7 @@
 let cardNum=prompt("karta raqamini kiriting")
-let cardSec = cardNum.slice(0, 4)+ "********" + cardNum.slice(12, 16);
 function cardNumber(cardNum){
-    if(cardNum.length===16 && !isNaN(+cardNum) || cardNum==null){
+    if(cardNum.length===16 && !isNaN(+cardNum) && cardNum==null){
+        let cardSec = cardNum.slice(0, 4)+ "********" + cardNum.slice(12);
         if(cardNum.startsWith("8600")){
             console.log("UzCard plastik ishlatasiz")
             console.log(cardSec)

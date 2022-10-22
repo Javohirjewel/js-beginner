7c3c5dc260032ed04357dbb26dee0eee2ed5d2d1
Javohirjewel <shdomonovjavohir56@gmail.com>
2022-10-22 15:24:24 +0500

    Finished array

---
+++ Arrays/main.js
@@ -0,0 +1,30 @@
+
+
+let club = ['Barcelona', 'PSJ', 'Manchester United', 'Real Madrid', 'Juventus', 'Machester City', 'Nasaf', 'Chelsea', 'Arsenal', 'Liverpool']
+
+
+function arrPush(str){
+    if(str){
+        club[club.length] = str
+        console.log(club)
+    }else{
+        console.log(club)
+    }
+}
+
+
+function arrPop(){
+    club.splice(club.length-1)
+    console.log(club)
+}
+
+arrPush("Atletico Madtid")
+arrPop()
+
+
+for(let i in club){
+    console.log(i + " " + club[i])
+}
+
+
+
